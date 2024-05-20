ae1ee78b14d03168ef0b5f43b3e2a7a86d328fa6
Amlend <amlendsingh@gmail.com>
2024-05-21 02:21:16 +0530

    minor-fix-for-team-validation

--- utils/validateTeam.js
+++ utils/validateTeam.js
@@ -10,29 +10,33 @@ const validateTeam = (players, captain, viceCaptain) => {
     BWL: 0,
   };
 
-  const playerRoles = {
-    WICKETKEEPER: "WK",
-    BATTER: "BAT",
-    "ALL-ROUNDER": "AR",
-    BOWLER: "BWL",
-  };
-
-  players.forEach((player) => {
-    const role = player.Role.toUpperCase();
-    const playerType = playerRoles[role];
-    if (playerType) {
-      playerTypes[playerType]++;
-    } else {
-      return false; // Invalid role
-    }
-  });
-
   const playerLimits = {
     WK: [1, 8],
     BAT: [1, 8],
     AR: [1, 8],
     BWL: [1, 8],
   };
+
+  const teamPlayerCounts = {};
+
+  players.forEach((player) => {
+    const role = player.Role.toUpperCase();
+    const playerType = role.substring(0, 3); // Extract the first 3 characters to get the player type
+    if (playerType in playerTypes) {
+      playerTypes[playerType]++;
+      const team = player.Team;
+      if (team in teamPlayerCounts) {
+        teamPlayerCounts[team]++;
+        if (teamPlayerCounts[team] > 10) {
+          return false; // More than 10 players selected from one team
+        }
+      } else {
+        teamPlayerCounts[team] = 1;
+      }
+    } else {
+      return false; // Invalid role
+    }
+  });
 
   for (const type in playerTypes) {
     if (
